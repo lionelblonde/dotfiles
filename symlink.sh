ef3204034e79221d461c93e58b7be78707eff82b
lionelblonde <lionel.blonde@gmail.com>
2019-04-17 12:51:18 +0900

    Remove redundant line already handled via condarc

--- symlink.sh
+++ symlink.sh
@@ -4,6 +4,7 @@
 ln -svf $HOME/dotfiles/.aliases $HOME/.aliases
 ln -svf $HOME/dotfiles/.bash_profile $HOME/.bash_profile
 ln -svf $HOME/dotfiles/.bashrc $HOME/.bashrc
+ln -svf $HOME/dotfiles/.condarc $HOME/.condarc
 ln -svf $HOME/dotfiles/.config/karabiner/karabiner.json $HOME/.config/karabiner/karabiner.json
 ln -svf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -svf $HOME/dotfiles/.hammerspoon/init.lua $HOME/.hammerspoon/init.lua
--- zsh.sh
+++ zsh.sh
@@ -9,6 +9,3 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 # Install Spaceship zsh theme
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-# Disable native conda prompt
-conda config --set changeps1 False
