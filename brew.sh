05f0724a4946243c488dedf37da0bb3069e036f2
lionelblonde <lionel.blonde@me.com>
2023-12-09 13:56:05 +0100

    Add ActivityWatch for automatic per-activity times

--- brew.sh
+++ brew.sh
@@ -45,6 +45,7 @@ brew install qmk/qmk/qmk
 brew install qmk-toolbox
 
 # Install macOS applications with brew cask
+brew install --cask activitywatch
 brew install --cask silicon
 brew install --cask kitty
 brew install --cask calibre
