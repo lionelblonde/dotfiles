1872a03ecd06e0a356e473a8004b98d46fbf6052
lionelblonde <lionel.blonde@me.com>
2024-07-29 23:51:58 +0200

    Remove mochi diffusion app

--- brew.sh
+++ brew.sh
@@ -55,7 +55,6 @@ brew install --cask silicon
 brew install --cask kitty
 brew install --cask calibre
 brew install --cask meetingbar
-brew install --cask mochi-diffusion
 brew install --cask tomatobar
 brew install --cask docker
 brew install --cask dash
