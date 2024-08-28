10c2b02ec04b1e4c69c44f0d9517d444afacdb1b
lionelblonde <lionel.blonde@me.com>
2024-08-28 21:05:31 +0200

    Add OmniFocus to hotkey-ed apps

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -89,7 +89,7 @@ hyperfns["="] = toggle_window_maximized
 
 -- Hotkeys to toggle focus on applications
 local hyperapps = {}
-hyperapps["y"] = ""
+hyperapps["y"] = "OmniFocus"
 hyperapps["u"] = "Finder"
 hyperapps["i"] = "kitty"
 hyperapps["p"] = "Dash"
