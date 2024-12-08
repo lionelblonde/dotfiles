86248ab2369c061859af3e42a48f9a5d65ebe1c4
lionelblonde <lionel.blonde@me.com>
2024-12-08 20:48:44 +0100

    Add calibre

--- brew.sh
+++ brew.sh
@@ -56,6 +56,7 @@ brew install --cask sioyek
 brew install --cask coteditor
 brew install --cask silicon
 brew install --cask kitty
+brew install --cask calibre
 brew install --cask meetingbar
 brew install --cask docker
 brew install --cask dash
