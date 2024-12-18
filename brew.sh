51499e7329f2f87379529c16a42eca53e571b502
lionelblonde <lionel.blonde@me.com>
2024-12-19 00:35:39 +0100

    Install activitywatch

--- brew.sh
+++ brew.sh
@@ -50,6 +50,7 @@ brew install qmk/qmk/qmk
 brew install qmk-toolbox
 
 # Install macOS applications with brew cask
+brew install --cask activitywatch
 brew install --cask git-credential-manager
 brew install --cask eloston-chromium
 brew install --cask tomatobar
