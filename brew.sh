d75981c63082d0774f7123bdfa71a99452b7b914
lionelblonde <lionel.blonde@me.com>
2025-02-11 17:57:11 +0100

    Remove skim

--- brew.sh
+++ brew.sh
@@ -62,7 +62,6 @@ brew install --cask silicon
 brew install --cask kitty
 brew install --cask calibre
 brew install --cask bike
-brew install --cask skim
 brew install --cask meetingbar
 brew install --cask dash
 brew install --cask ivpn
