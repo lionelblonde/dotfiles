62f67727c772f1ece2e0e5778f74383a57f532d5
lionelblonde <lionel.blonde@me.com>
2025-02-23 10:58:15 +0100

    Re-org files for better legibility

--- symlink.sh
+++ symlink.sh
@@ -8,12 +8,15 @@
 ln -svf $HOME/Documents/dotfiles/.aliases $HOME/.aliases
 ln -svf $HOME/Documents/dotfiles/.bash_profile $HOME/.bash_profile
 ln -svf $HOME/Documents/dotfiles/.bashrc $HOME/.bashrc
+
 cd $HOME/.config && mkdir -p nvim
 ln -svf $HOME/Documents/dotfiles/.config/nvim/init.lua $HOME/.config/nvim/init.lua
-ln -svf $HOME/Documents/dotfiles/.gitconfig $HOME/.gitconfig
+
 cd $HOME
 mkdir -p .hammerspoon
 ln -svf $HOME/Documents/dotfiles/.hammerspoon/init.lua $HOME/.hammerspoon/init.lua
+
+ln -svf $HOME/Documents/dotfiles/.gitconfig $HOME/.gitconfig
 ln -svf $HOME/Documents/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -svf $HOME/Documents/dotfiles/.vimrc $HOME/.vimrc
 ln -svf $HOME/Documents/dotfiles/.zshrc $HOME/.zshrc
