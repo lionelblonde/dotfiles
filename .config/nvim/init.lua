dc0034ea0a80e1ba521c1b9c6af402524f1fd62b
lionelblonde <lionel.blonde@me.com>
2025-09-28 13:44:16 +0200

    Remove vimtex stuff entirely (using Overleaf exclusively now)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -218,21 +218,6 @@ require("lazy").setup({
         end
     },
     {
-        "lervag/vimtex",
-        config = function()
-            vim.g.tex_flavor = "latex"
-            vim.g.vimtex_view_method = "skim"
-            vim.g.vimtex_callback_progpath = "nvr"
-            vim.g.vimtex_view_skim_sync = 1
-            -- value 1 allows forward search after every successful compilation
-            vim.g.vimtex_view_skim_activate = 1
-            -- value 1 allows change focus to the viewer after command `:VimtexView` is given
-            vim.g.vimtex_quickfix_mode = 0
-            vim.g.tex_conceal = "abdmg"
-            vim.g.vimtex_imaps_leader = ";" -- default is backtick, which is an annoyance
-        end
-    },
-    {
         "kylechui/nvim-surround", -- surround objects
         config = function()
             require("nvim-surround").setup({})
