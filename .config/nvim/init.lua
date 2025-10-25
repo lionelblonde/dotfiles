f30f7155d5fb82cc67077df53b526d8d014be716
lionelblonde <lionel.blonde@me.com>
2025-10-25 14:42:25 +0200

    Remove telescope symbol search (not useful)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -124,10 +124,6 @@ require("lazy").setup({
                 local word = vim.fn.expand("<cWORD>") -- the WORD under the cursor
                 builtin.grep_string({ search = word })
             end)
-            -- search symbols in current buffer
-            vim.keymap.set("n", "<leader>ss", builtin.lsp_document_symbols)
-            -- search symbols in workspace (according to LSP)
-            vim.keymap.set("n", "<leader>sS", builtin.lsp_workspace_symbols)
         end
     },
     {
@@ -461,12 +457,13 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover({ border = "rounded" }) end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
         vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         -- The two following keymaps are handled via trouble
         -- vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
         -- vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
