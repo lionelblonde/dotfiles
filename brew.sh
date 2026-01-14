31e343c8a741f8ccb6b9c869ccc15fe555ef107f
lionelblonde <lionel.blonde@me.com>
2026-01-14 18:07:59 +0100

    Add back itsycal (for the hourly chime mostly)

--- brew.sh
+++ brew.sh
@@ -59,6 +59,7 @@ brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask font-iosevka-term-nerd-font
 brew install --cask git-credential-manager
 brew install --cask monarch
+brew install --cask itsycal
 brew install --cask alt-tab
 brew install --cask little-snitch
 brew install --cask hazeover
