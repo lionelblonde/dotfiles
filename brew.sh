9731e1cf8d5b55c51eaf4eeed6ccc1628ea5620c
lionelblonde <lionel.blonde@me.com>
2026-07-19 11:14:10 +0200

    add convenient calculator app

--- brew.sh
+++ brew.sh
@@ -66,6 +66,7 @@ brew install --cask monarch
 brew install --cask handbrake
 brew install --cask little-snitch
 brew install --cask google-chrome
+brew install --cask numi
 brew install --cask maccy
 brew install --cask shottr
 brew install --cask skim
